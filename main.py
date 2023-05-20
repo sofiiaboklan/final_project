9c4cff7faafe4d46d3c1018e8c6769609a52ce7a
sofiiaboklan <124063273+sofiiaboklan@users.noreply.github.com>
2023-05-20 20:36:53 +0100

    initial cart

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from aiogram.types import Message, CallbackQuery, KeyboardButton, ReplyKeyboardM
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram import F
 from aiogram.types.update import Update
+from pymongo import ReturnDocument
 
 
 # This function initializes the database connection and returns the collection object for further use.
@@ -24,16 +25,21 @@ def initDB():
     myItems = [{"_id": "A111A1", "name": "Кеди converse оригінал", "size": "36.5 / 23 см", "price": "300",
                 "availability": True},
                {"_id": "A111A2", "name": "Чорний кроп-топ", "size": "S", "price": "150",
-                "availability": False}]
+                "availability": False},
+               {"_id": "A111A3", "name": "Чорний кроп-топ", "size": "S", "price": "150",
+                "availability": True}
+               ]
 
-    myOrders = [{"order_id": "1", "item_id": "A111A2", "customer_info": "Софія Боклан, Київ, 304",
-                 "username": "sofiiaboklan", "order_status": "awaiting for confirmation", "order_complete": False}]
+    myOrders = [{"order_id": "1", "item_id": ["A111A2"], "customer_info": "Софія Боклан, Київ, 304",
+                 "username": "sofiiaboklaan", "order_status": "Очікує підтвердження оплати.", "order_complete": False}]
 
-    if mycol.find(myItems[0]) is None:
+    mycol.drop()
+    mycol1.drop()
+    if mycol.estimated_document_count() == 0:
         mycol.insert_many(myItems)
 
-    if mycol1.find(myOrders[0]) is None:
-        mycol.insert_many(myOrders)
+    if mycol1.estimated_document_count() == 0:
+        mycol1.insert_many(myOrders)
 
     return mycol, mycol1
 
@@ -43,7 +49,8 @@ def init_keyboard():
     button_order = KeyboardButton(text="Зробити замовлення")
     button_track_order = KeyboardButton(text="Статус замовлення")
     button_admin = KeyboardButton(text="Звʼязатися з нами")
-    keyboard = ReplyKeyboardMarkup(keyboard=[[button_order], [button_track_order], [button_admin]],
+    button_cart = KeyboardButton(text="Мій кошик")
+    keyboard = ReplyKeyboardMarkup(keyboard=[[button_order], [button_cart], [button_track_order], [button_admin]],
                                    resize_keyboard=True, row_width=1, is_persistent=True)
 
     return keyboard
@@ -57,6 +64,8 @@ router = Router()
 
 # a variable to initialize the database connection and returns the collection object for further use.
 itemsCollection, ordersCollection = initDB()
+
+my_cart = []
 
 
 # initial command ("/start"), sends a greeting message to the user and initializes a custom keyboard
@@ -72,24 +81,78 @@ itemsCollection, ordersCollection = initDB()
 
 
 class MyCallback(CallbackData, prefix="my"):
-    entering: bool
+    code: str
+    code = None
+    state: str
+
+
+# state is entering, ordering, menu
 
 
 class Form(StatesGroup):
     put_code = State()
+    put_personal_data = State()
+
+
+@router.callback_query(MyCallback.filter(F.state == "ordering"))
+async def order_callback_foo(query: CallbackQuery, state: FSMContext):
+    await state.set_state(Form.put_personal_data)
+    # await state.update_data(put_code=query.data.split(':')[1])
+    await query.message.answer("<b>Реквізити для оплати:</b> \nОтримувач: Боклан Софія\nМонобанк: 4441 1144 2342 3837"
+                               "\nПісля цього напишіть, будь ласка, свої <b>реквізити для відправки</b> у форматі:"
+                               "\nПІБ, номер телефону, місто, номер відділення нової пошти.")
+
+
+@router.callback_query(MyCallback.filter(F.state == "cart"))
+async def order_callback_foo(query: CallbackQuery, state: FSMContext):
+    my_cart.append(query.data.split(':')[1])
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Перейти до оплати", callback_data=MyCallback(state="ordering").pack())
+    builder.button(text="Додати ще одну річ в кошик", callback_data=MyCallback(state="menu").pack())
+    builder.adjust(1, 2)
+    await query.message.answer("Річ додано в кошик!", reply_markup=builder.as_markup())
 
 
 # це як блять каунтер нахуй
-@router.callback_query(MyCallback.filter(F.entering == True))
+@router.callback_query(MyCallback.filter(F.state == "entering"))
 async def my_callback_foo(query: CallbackQuery, state: FSMContext):
     await state.set_state(Form.put_code)
     await query.message.answer("Введіть артикль речі")
 
 
-@router.callback_query(MyCallback.filter(F.entering == False))
+@router.callback_query(MyCallback.filter(F.state == "menu"))
 async def my_callback_foo(query: CallbackQuery, state: FSMContext):
     await state.clear()
     await echo_handler(query.message)
+
+
+####################
+@router.message(Form.put_personal_data)
+async def put_personal_data_handler(message: Message, state: FSMContext) -> None:
+    # data = await state.get_data()
+    # item_id = data["put_code"]
+    # await state.update_data(put_personal_data=message.text)
+
+    personal_info = message.text
+    order = {"order_id": ordersCollection.estimated_document_count() + 1, "item_id": my_cart,
+             "customer_info": personal_info,
+             "username": message.from_user.username, "order_status": "Очікує підтвердження оплати.",
+             "order_complete": False}
+
+    for item_id in my_cart:
+        itemsCollection.find_one_and_update({'_id': item_id}, {'$set': {"availability": False}})
+
+    ordersCollection.insert_one(order)
+
+    my_cart.clear()
+
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Головна сторінка", callback_data=MyCallback(state="menu").pack())
+    builder.adjust(1, 2)
+    await message.answer("Дякуємо! Очікуйте на підтвердження замовлення протягом доби.",
+                         reply_markup=builder.as_markup())
+
+    await state.clear()
 
 
 @router.message(Form.put_code)
@@ -98,20 +161,30 @@ async def put_code_handler(message: Message, state: FSMContext) -> None:
     code = message.text
     myquery = {"_id": code}
     item = itemsCollection.find_one(myquery)
+
     if item is None:
-        await message.answer("Ми не знайшли речі за таким артиклем. Переконайтесь, що ввели його правильно, "
-                             "наприклад АА111А")
+        await message.answer(f"Ми не знайшли речі за таким артиклем. Переконайтесь, що ввели його правильно, "
+                             "наприклад <b>АА111А</b>")
     else:
         if item["availability"] is False:
             builder = InlineKeyboardBuilder()
-            builder.button(text="Так", callback_data=MyCallback(entering=True).pack())
-            builder.button(text="Головна сторінка", callback_data=MyCallback(entering=False).pack())
+            builder.button(text="Так", callback_data=MyCallback(state="entering").pack())
+            builder.button(text="Головна сторінка", callback_data=MyCallback(state="menu").pack())
             builder.adjust(1, 2)
             await message.answer("Ця річ більше не в наявності :( \nБажаєте придбати іншу річ?",
                                  reply_markup=builder.as_markup())
         else:
-            # print(message.from_user.username)
-            await message.answer(item["name"])
+            builder = InlineKeyboardBuilder()
+            builder.button(text="Додати в кошик", callback_data=MyCallback(state="cart", code=code).pack())
+            builder.button(text="Придбати зараз", callback_data=MyCallback(state="ordering", code=code).pack())
+            builder.button(text="Головна сторінка", callback_data=MyCallback(state="menu").pack())
+            builder.adjust(1, 2)
+            await message.answer(item["name"] +
+                                 "\n\n" +
+                                 item["price"] +
+                                 " грн" +
+                                 "\n\n" +
+                                 item["_id"], reply_markup=builder.as_markup())
             await state.clear()
 
 
@@ -119,8 +192,8 @@ async def put_code_handler(message: Message, state: FSMContext) -> None:
 @router.message(F.text == 'Зробити замовлення')
 async def command_place_order_handler(message: Message) -> None:
     builder = InlineKeyboardBuilder()
-    builder.button(text="Ввести артикль речі", callback_data=MyCallback(entering=True).pack())
-    builder.button(text="Головна сторінка", callback_data=MyCallback(entering=False).pack())
+    builder.button(text="Ввести артикль речі", callback_data=MyCallback(state="entering").pack())
+    builder.button(text="Головна сторінка", callback_data=MyCallback(state="menu").pack())
     builder.adjust(1, 2)
     await message.answer("\u2B07\uFE0F \u2B07\uFE0F \u2B07\uFE0F", reply_markup=builder.as_markup())
 
@@ -129,6 +202,39 @@ async def command_place_order_handler(message: Message) -> None:
 @router.message(F.text == 'Звʼязатися з нами')
 async def command_contact_handler(message: Message) -> None:
     await message.answer(text="Контакти адміністраторки:\n@sofiiaboklan / +380663343593. \nРобочі години:\n10:00-20:00")
+
+
+@router.message(F.text == 'Статус замовлення')
+async def command_status_handler(message: Message) -> None:
+    myquery = {"username": message.from_user.username}
+    item = ordersCollection.find_one(myquery)
+
+
+
+    await message.answer(text=f"Замовлення №{item['order_id']} \n{item['order_status']}")
+
+
+# МІЙ КОШИК
+@router.message(F.text == 'Мій кошик')
+async def command_status_handler(message: Message) -> None:
+    items = []
+    for item_id in my_cart:
+        myquery = {"_id": item_id}
+        items.append(itemsCollection.find_one(myquery))
+
+    response = ""
+
+    for item in items:
+        response += f"№{item['_id']} + name: {item['name']}"
+    if response == "":
+        response = "nothing there"
+    await message.answer(text=response)
+
+    # print(items.next())
+    # # while items.next()
+    # #     response += f"Замовлення №{item['order_id']} \n{item['order_status']}"
+    #
+    # await message.answer(text=response)
 
 
 # message here is a message, sent by user, overall message
